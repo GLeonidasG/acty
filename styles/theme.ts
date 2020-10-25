3b1fb381fba3a7fafee4e1f550381f6ff4efe2e7
Genario Leonidas Goetz <genario.goetz@meta.com.br>
2020-10-24 21:34:12 -0300

    initial commit

---
+++ styles/theme.ts
@@ -0,0 +1,35 @@
+import { theme, DefaultTheme } from "@chakra-ui/core";
+
+const customTheme: DefaultTheme = {
+  ...theme,
+  fonts: {
+    heading: `DM Serif Text, sans-serif`,
+    body: `Open Sans, sans-serif`,
+    mono: "Roboto Mono, sans-serif",
+  },
+  colors: {
+    ...theme.colors,
+    gray: {
+      ...theme.colors.gray,
+      "900": "#303030",
+    },
+    green: {
+      ...theme.colors.green,
+      "600": "#00BF9D",
+    },
+    blue: {
+      ...theme.colors.blue,
+      "600": "#2D9CDB",
+    },
+    cyan: {
+      ...theme.colors.cyan,
+      "400": "#48F4FF",
+    },
+    teal: {
+      ...theme.colors.teal,
+      "300": "#48FFDE",
+    },
+  },
+};
+
+export default customTheme;
